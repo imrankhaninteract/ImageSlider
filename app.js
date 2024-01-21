daf2b21bc78ad459625cbd46cf2aee8ad6f0d30d
imrankhan <imrankhaninteract@gmail.com>
2024-01-21 16:23:18 +0600

    styleAndFunctionChanged

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-const slides = document.querySelectorAll(".slide")
-var counter = 0;
-// console.log(slides)
-slides.forEach(
-    (slide, index) => {
-        slide.style.top = `${index * 100}%`
-    }
-)
+const slides = document.querySelectorAll(".slide");
+let counter = 0;
+
+slides.forEach((slide, index) => {
+    slide.style.top = `${index * 100}%`;
+});
 
 const goPrev = () => {
-    counter--
-    slideImage()
-}
+    counter = (counter - 1 + slides.length) % slides.length;
+    slideImage();
+};
 
 const goNext = () => {
-    counter++
-    slideImage()
-}
+    counter = (counter + 1) % slides.length;
+    slideImage();
+};
 
 const slideImage = () => {
-    slides.forEach(
-        (slide) => {
-            slide.style.transform = `translateY(-${counter * 100}%)`
-        }
-    )
-}
+    slides.forEach((slide) => {
+        slide.style.transform = `translateY(-${counter * 100}%)`;
+    });
+};
